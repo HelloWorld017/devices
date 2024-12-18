4969465187de3627af14fc04f5b74d64cb242efd
nenw <khi@nenw.dev>
2024-12-19 03:15:07 +0900

    feat(fonts): add fontconfig

--- modules/packages/fonts/default.nix
+++ modules/packages/fonts/default.nix
@@ -1,12 +1,24 @@
-{ config, lib, pkgs, ... }:
+{ options, lib, pkgs, ... }:
 
 {
-	config = {
-		fonts.packages = with pkgs; [
-			(pkgs.callPackage ./sarasa-gothic.nix {})
-			(pkgs.callPackage ./metropolis.nix {})
-			pretendard
-			pretendard-jp
-		];
-	};
+	config = lib.mkMerge [
+		(if (builtins.hasAttr "fontconfig" options.fonts) then {
+			fonts.fontconfig = {
+				enable = true;
+				defaultFonts = {
+					serif = [ "Pretendard JP" ];
+					sansSerif = [ "Pretendard JP" ];
+					monospace = [ "Sarasa Term K Nerd Font" ];
+				};
+			};
+		} else {})
+		{
+			fonts.packages = with pkgs; [
+				(pkgs.callPackage ./sarasa-gothic.nix {})
+				(pkgs.callPackage ./metropolis.nix {})
+				pretendard
+				pretendard-jp
+			];
+		}
+	];
 }
