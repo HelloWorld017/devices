7b3456841195c84c6e7dbc3d1d75a81e324e9d8d
nenw <khi@nenw.dev>
2025-02-01 12:31:48 +0900

    feat(zsh): migrate to zdharma-continuum

--- modules/packages/zsh/assets/init.zsh
+++ modules/packages/zsh/assets/init.zsh
@@ -7,7 +7,6 @@ setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
-# setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
 
@@ -17,15 +16,16 @@ bindkey "^[[F"	end-of-line
 bindkey "^[[3~"	delete-char
 
 # Start zinit
-if [[ ! -f $HOME/.zinit/bin/zi.zsh ]]; then
-	print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+declare -A ZINIT
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+	print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
 	command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-	command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+	command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
 		print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
 		print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 
-source "$HOME/.zinit/bin/zi.zsh"
+source "$HOME/.zinit/bin/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
@@ -44,7 +44,7 @@ zinit light jeffreytse/zsh-vi-mode
 # > Autocompletion, Suggestions, Syntax Highlight
 zinit wait lucid light-mode for \
 	atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
-		zdharma/fast-syntax-highlighting \
+		zdharma-continuum/fast-syntax-highlighting \
 	blockf \
 		zsh-users/zsh-completions \
 	atload"!_zsh_autosuggest_start" \
@@ -52,7 +52,7 @@ zinit wait lucid light-mode for \
 
 # > History Search
 zinit wait lucid light-mode for \
-	zdharma/history-search-multi-word
+	zdharma-continuum/history-search-multi-word
 
 # Fuzzy Finder
 export FZF_DEFAULT_COMMAND='fd --type f'
