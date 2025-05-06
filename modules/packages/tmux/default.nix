f60ee023f8f99bc4bb226bc4ffc378137d9e76b3
nenw <khi@nenw.dev>
2025-05-06 15:05:23 +0000

    fix(tmux): use fa instead of mdi icon in nerdfont

--- modules/packages/tmux/default.nix
+++ modules/packages/tmux/default.nix
@@ -36,10 +36,10 @@
 					extraConfig = ''
 						# Config powerline
 						set -g @tmux_power_theme 'moon'
-						set -g @tmux_power_user_icon ' ' # ' ﱤ '
-						set -g @tmux_power_session_icon '  '
-						set -g @tmux_power_time_icon '  '
-						set -g @tmux_power_date_icon '  ' # '  '
+						set -g @tmux_power_user_icon ' '
+						set -g @tmux_power_session_icon '  '
+						set -g @tmux_power_time_icon '  '
+						set -g @tmux_power_date_icon '  ' # '  '
 						set -g @tmux_power_left_arrow_icon '​'
 						set -g @tmux_power_right_arrow_icon '​'
 					'';
