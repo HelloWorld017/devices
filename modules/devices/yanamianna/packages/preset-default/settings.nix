497489c973edb4710cee537ac7c68bd91f78a98d
nenw <khi@nenw.dev>
2025-06-24 11:59:11 +0900

    feat(yanamianna): support ntfs

--- modules/devices/yanamianna/packages/preset-default/settings.nix
+++ modules/devices/yanamianna/packages/preset-default/settings.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
 	boot.kernelPackages = pkgs.linuxPackages_latest;
+	boot.supportedFilesystems = [ "ntfs" ];
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
