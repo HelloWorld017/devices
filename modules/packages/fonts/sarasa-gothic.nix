35ac5364559872a9f4e5e22d6cd290af3ef7324b
nenw <khi@nenw.dev>
2025-09-25 22:47:02 +0900

    fix(fonts): fix broken sarasa gothic

--- modules/packages/fonts/sarasa-gothic.nix
+++ modules/packages/fonts/sarasa-gothic.nix
@@ -1,12 +1,12 @@
 { lib, fetchzip }:
 
 let
-	version = "0.37.4-1";
+	version = "1.0.33-0";
 in fetchzip {
 	name = "sarasa-gothic-nerd-font-${version}";
 
 	url = "https://github.com/jonz94/Sarasa-Gothic-Nerd-Fonts/releases/download/v${version}/sarasa-term-k-nerd-font.zip";
-	sha256 = "6c7a4b7a0b70cba42801f6bc8cf127cc5a349ebacd35871802e4c2f30f9b64ea";
+	hash = "sha256-YTdcUevg59+3C5VePHsqSJu07t+wpqEWmOk9dwJV8D4=";
 	stripRoot = false;
 
 	postFetch = ''
