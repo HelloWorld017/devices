46fba2fc8c596cc45a036896a25e0fff5d8ad3b3
nenw <khi@nenw.dev>
2025-11-05 01:25:57 +0000

    feat(keys): add akebi key

--- keys.nix
+++ keys.nix
@@ -6,6 +6,7 @@ let
 		# > DO NOT ADD ANY UNTRUSTED KEYS HERE.
 
 		"user-nenw-ajisai" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIY/EwgzMYfp/FEUUizTWjw++53LZAxmJ4WgHVA4YiVY";
+		"user-nenw-akebi" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWzjPz2+WzZd4IjSoeaSSSvXizSObmCxg+KMp+/V/jG";
 		"user-nenw-shigureui" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkd7Fk7lI0Sdh5i0U2vI1AUTlQo+pVKghxLW4vJ17/D";
 	};
 in (
