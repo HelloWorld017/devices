9c0d4a6c616ca6e63312277d69c24cb1f020e2e5
nenw <khi@nenw.dev>
2025-11-12 02:36:17 +0900

    feat(git): add some configs

--- modules/packages/git/default.nix
+++ modules/packages/git/default.nix
@@ -11,10 +11,14 @@
 				alias.bundlebranch = "!f() { git bundle create \"$2\" \"$1\" ^$(git merge-base \"$1\" \"master\"); }; f";
 				alias.bundlestash = "!f() { git tag git-bundlestash-tag \"stash@{$1}\"; git bundle create \"$2\" git-bundlestash-tag ^master; git tag -d git-bundlestash-tag; }; f";
 				alias.l = "!sh -c 'PAGER=\"less -FRXK +/HEAD\" git log --oneline --graph --decorate'";
+				branch.sort = "-committerdate";
 				checkout.defaultRemote = "origin";
+				diff.algorithm = "histogram";
 				pager.diff = "delta --plus-style 'syntax #205820'";
 				pull.ff = "only";
 				push.autoSetupRemote = "true";
+				merge.conflictstyle = "zdiff3";
+				rebase.autosquash = "true";
 				rerere.enabled = "true";
 			};
 		};
