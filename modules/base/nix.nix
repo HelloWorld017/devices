fc6a39f09be655ca149de042f823eb24153ea8cd
nenw <khi@nenw.dev>
2026-04-09 22:44:53 +0900

    feat: allow wheel to build nix

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -4,7 +4,7 @@
 		# Nix Configuration
 		nix.settings.substituters = [ "https://cache.nixos.org/" ];
 		nix.settings.trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ]; 
-		nix.settings.trusted-users = [ "@admin" ];
+		nix.settings.trusted-users = [ "@admin" "@wheel" ];
 		nix.settings.experimental-features = ["nix-command" "flakes"];
 		nix.settings.auto-optimise-store = true;
 
