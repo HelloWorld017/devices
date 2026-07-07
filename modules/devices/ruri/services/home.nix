f8e36845a1fb2e5d1164a4c5242cd3a29aa98704
nenw <khi@nenw.dev>
2026-07-07 11:29:59 +0900

    feat: add home service for ruri

---
+++ modules/devices/ruri/services/home.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... }: let
+  inherit (lib) concatMap listToAttrs map nameValuePair;
+
+  ports = config.pkgs.server.ports.ports;
+
+  pagesHost = "pages.nenw.dev";
+  pagesRoutes = [ "bits" "irodori" "tone-marble" ];
+  pagesProxy = {
+    proxyPass = "https://${pagesHost}";
+    extraConfig = ''
+      proxy_set_header Host ${pagesHost};
+      proxy_ssl_server_name on;
+      proxy_ssl_name ${pagesHost};
+      proxy_redirect https://${pagesHost}/ https://$host/;
+      proxy_redirect https://${pagesHost} https://$host;
+    '';
+  };
+
+  pagesLocation = listToAttrs (map
+    (name: nameValuePair name pagesProxy)
+    (concatMap (route: [ "= /${route}" "/${route}/" ]) pagesRoutes)
+  );
+in {
+  config = {
+    pkgs.server = {
+      ingress.rules."nenw.dev" = {
+        acmeHost = "nenw.dev";
+        locations = pagesLocation // {
+          "/" = pagesProxy;
+
+          "= /.well-known/openid-configuration" = {
+            proxyPass = "http://127.0.0.1:${toString ports.auth}";
+          };
+
+          "= /.well-known/webfinger" = {
+            proxyPass = "http://127.0.0.1:${toString ports.misskey}";
+          };
+        };
+      };
+    };
+  };
+}
