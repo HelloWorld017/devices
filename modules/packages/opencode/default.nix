c52473cd5b5c7455caca8d464dbef662f45f8da0
nenw <khi@nenw.dev>
2026-09-24 13:34:20 +0000

    chore(opencode): update config

--- modules/packages/opencode/default.nix
+++ modules/packages/opencode/default.nix
@@ -1,7 +1,19 @@
-{ pkgs, inputs, system, ... }:
+{ pkgs, inputs, system, lib, ... }:
 
 let
-  tuiSettings = {
+  inherit (lib) listToAttrs map nameValuePair;
+
+  stores = [ "~/.cargo/**" "~/.pnpm/**" "/nix/store/**" ];
+  config = {
+    autoupdate = false;
+    snapshots = false;
+    permissions = {
+      external_directory = listToAttrs (map (dir: nameValuePair dir "allow") stores);
+      edit = listToAttrs (map (dir: nameValuePair dir "deny") stores);
+    };
+  };
+
+  tuiConfig = {
     "input_submit" = "return";
     "input_newline" = "shift+return";
     "theme" = "cursor";
@@ -12,12 +24,11 @@ in {
       inputs.llm-agents.packages.${system}.opencode
     ];
 
-    home.configFile = {
-      "opencode/tui.json" = let
-        json = pkgs.formats.json { };
-      in {
-        source = json.generate "tui.json" tuiSettings;
-      };
+    home.configFile = let
+      json = pkgs.formats.json { };
+    in {
+      "opencode/tui.json".source = json.generate "tui.json" tuiConfig;
+      "opencode/opencode.json".source = json.generate "opencode.json" config;
     };
   };
 }
